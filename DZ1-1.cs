2355f97aa899a8f13a203236e515b5e05aa7a3fa
Анастасия Кутовая <80527429+Anastasiy111@users.noreply.github.com>
2021-03-13 12:32:30 +0700

    Добавили файлы DZ1-1 и DZ1-2

---
+++ DZ1-1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace DZ1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_manth; 
+            float flag;
+            do
+            {
+                Console.Write("Введите номер месяца: ");
+                number_manth = int.Parse(Console.ReadLine());
+                if ((number_manth >= 1) & (number_manth <= 12))
+                {
+                    switch (number_manth)
+                    {
+                        case 1:
+                            Console.WriteLine("Январь");
+                            break;
+                        case 2:
+                            Console.WriteLine("Февраль");
+                            break;
+                        case 3:
+                            Console.WriteLine("Март");
+                            break;
+                        case 4:
+                            Console.WriteLine("Апрель");
+                            break;
+                        case 5:
+                            Console.WriteLine("Май");
+                            break;
+                        case 6:
+                            Console.WriteLine("Июнь");
+                            break;
+                        case 7:
+                            Console.WriteLine("Июль");
+                            break;
+                        case 8:
+                            Console.WriteLine("Август");
+                            break;
+                        case 9:
+                            Console.WriteLine("Сентябрь");
+                            break;
+                        case 10:
+                            Console.WriteLine("Октябрь");
+                            break;
+                        case 11:
+                            Console.WriteLine("Ноябрь");
+                            break;
+                        case 12:
+                            Console.WriteLine("Декабрь");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка. Введено не верное число!");
+                }
+                Console.Write("Для прекращения работы программы введите 0, иначе любое число: ");
+                flag = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+            } while (flag != 0);
+            Console.WriteLine("До свидания!");
+        }
+    }
+}
---
+++ DZ1-2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DZ1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flag = 1;
+            do
+            {
+                Console.WriteLine("Введите последовательность символов (С-с, Ю-ю, З-з, В-в)");
+                string way = Console.ReadLine();
+                char[] way_mas = way.ToCharArray();
+
+                int x = 0, y = 0, q=1;
+                for (int i = 0; i < way_mas.Length; i++)
+                {
+                    switch (way[i])
+                    {
+                        case 'С': break;
+                        case 'с': break;
+                        case 'Ю': break;
+                        case 'ю': break;
+                        case 'В': break;
+                        case 'в': break;
+                        case 'З': break;
+                        case 'з': break;
+                        default: q = 0; break;
+                    }
+                }
+
+                if (q == 0)
+                {
+                    Console.WriteLine("Ошибка. Введен неверный символ(ы)!");
+                }
+                else if (q == 1)
+                {
+                    for (int i = 0; i < way_mas.Length; i++)
+                    {
+                        switch (way[i])
+                        {
+                            case 'С': y = y + 1; break;
+                            case 'с': y = y + 1; break;
+                            case 'Ю': y = y - 1; break;
+                            case 'ю': y = y - 1; break;
+                            case 'В': x = x + 1; break;
+                            case 'в': x = x + 1; break;
+                            case 'З': x = x - 1; break;
+                            case 'з': x = x - 1; break;
+                        }
+
+                    }
+                    Console.WriteLine($"x={x}, y={y}");
+                    double distance = Math.Sqrt(x * x + y * y);
+                    Console.WriteLine($"Растояние между началом и концом составило: {distance}");
+                }            
+                                
+                Console.Write("Для прекращения работы программы ввдите 0, иначе любое число: ");
+                flag = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+            } while (flag != 0);
+            Console.WriteLine("До свидания!");
+        }
+    }
+}
